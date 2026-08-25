38da57f663f4c7350db32657ad7a4281f890b5c4
Acirio Gabriel <agabrielfb11@gmail.com>
2026-08-25 00:32:09 -0300

    refactor: Mudanca na dinamica da missao e adicao de xp e item a recompensa, para que seja possivel recompensar o aventureiro durante as missoes. Cada missao tambem tera um nivel necessario para ser realizada.

--- src/Inventory/Inventory.java
+++ src/Inventory/Inventory.java
@@ -2,8 +2,29 @@ package src.Inventory;
 
 import src.Item.Item;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Inventory {
     private List<Item> items;
+
+    public Inventory() {
+        this.items = new ArrayList<>();
+    }
+
+    public void getItems() {
+        if(items.isEmpty()){
+            System.out.println("O invetáro está vazio.\n");
+            return;
+        }
+
+        for(int i = 0; i < items.size(); i++){
+            System.out.println((i+1) + ". " + items.get(i).getName() + "\n"); // "1. Nome do item"
+        }
+    }
+
+    public void addItem(Item item) {
+        items.add(item);
+    }
 }
--- src/Reward/Reward.java
+++ src/Reward/Reward.java
@@ -1,15 +1,20 @@
 package src.Reward;
 
+import src.Item.Item;
+
 public class Reward {
     private final String description;
-    private int energy;
-    private int level;
+    private Item item;
+    private int experience;
 
-    public Reward(String description, int energy) {
+
+    public Reward(String description, Item item, int experience) {
         this.description = description;
-        this.energy = energy;
+        this.item = item;
+        this.experience = experience;
     }
 
     public String getDescription() { return description; }
-    public int getEnergy() { return energy; }
+    public Item getItem() { return item; }
+    public int getExperience() { return experience; }
 }
